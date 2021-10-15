10ac78de6995cfc996c44f45aa0a1302dc6c890f
ConorWeldon <conorw177@gmail.com>
2021-10-15 13:48:45 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,3 +6,46 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+import * as THREE from 'three';
+// import { OrbitControls } from 'three';
+// import { FlakesTexture } from 'three';
+// import { RGBELoader } from 'three';
+
+    // const scene = new THREE.Scene();
+
+    let scene, camera, renderer, controls, pointlight;
+    function init() {
+        scene = new THREE.Scene();
+
+        //set scene background to transparent so can see the background
+        renderer = new THREE.WebGLRenderer({alpha:true,antialias:true});
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+        camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 1, 1000);
+        camera.position.set(0, 0, 500);
+        controls = new OrbitControls(camera, renderer.domElement);
+
+        //LIGHTS
+        pointlight = new THREE.PointLight(0xffffff, 1);
+        pointlight.position.set(200, 200, 200);
+        scene.add(pointlight);
+
+        //SPHERE
+        let ballGeo = new THREE.SphereGeometry(100, 64, 64);
+        let ballMat = new THREE.MeshPhysicalMaterial();
+        let ballMesh = new THREE.Mesh(ballGeo, ballMat);
+        scene.add(ballMesh);
+
+        animate(); //Rendering my loop
+    }
+
+    function animate() {
+        controls.update();
+        renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+    }
+
+    //To start Scene
+    init();
